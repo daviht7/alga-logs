e68d25104020bbc104cbd0bbc37039b8fad71d99
Davi Holanda <daviht7@gmail.com>
2022-05-21 00:31:52 -0300

    criação de model Cliente e Controller Cliente

---
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -0,0 +1,26 @@
+package com.algaworks.algalog.api.controller;
+
+import com.algaworks.algalog.domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @GetMapping("/clientes")
+    public List<Cliente> listar() {
+        var cliente1 = new Cliente();
+
+        cliente1.setId(1L);
+        cliente1.setNome("João");
+        cliente1.setEmail("joao@gmail.com");
+        cliente1.setTelefone("85994185335");
+
+        return Arrays.asList(cliente1);
+
+    }
+
+}
