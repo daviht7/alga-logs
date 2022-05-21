358e08f10ecc3082ccd317ccc8baac7bfe871ce1
Davi Holanda <daviht7@gmail.com>
2022-05-21 14:28:39 -0300

    cliente repository

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,10 @@
 package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
@@ -12,14 +15,17 @@ import java.util.List;
 @RestController
 public class ClienteController {
 
-    @PersistenceContext
-    private EntityManager entitymanager;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
 
-        return entitymanager.createQuery("from Cliente",Cliente.class).getResultList();
-
+    @GetMapping("/clientes/{clienteId}")
+    public Cliente buscar(@PathVariable Long clienteId) {
+        return clienteRepository.findById(clienteId).get();
     }
 
 }
